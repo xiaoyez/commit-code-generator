5177f14a83e5e39522ffc0c1a938940847b5ee98
Crescent Moon <t565022817@outlook.com>
2023-04-13 10:57:18 +0800

    test

--- src/definitions/api/TypeDefinition.ts
+++ src/definitions/api/TypeDefinition.ts
@@ -1,12 +1,13 @@
 import { JavaType } from './JavaType';
+import { ObjectTypeDefinition } from './ObjectTypeDefinition';
 interface ITypeDefinition {
-    type: JavaType;
+    type: JavaType|ObjectTypeDefinition;
     genericTypes?: ITypeDefinition[];
 
 }
 
 export class TypeDefinition implements ITypeDefinition {
-    type!: JavaType;
+    type!: JavaType|ObjectTypeDefinition;
     genericTypes?: ITypeDefinition[];
 
 
@@ -15,3 +16,15 @@ export class TypeDefinition implements ITypeDefinition {
     }
 
 }
+
+type N = {
+    aaa: number;
+    bbb: string;
+}
+
+type K<T,L> = {
+    data: T;
+    base222: L;
+}
+
+type Use = K<N, N>;
