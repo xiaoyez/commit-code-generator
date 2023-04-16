92a3b3da7a7c6b09352ee576155c2e372a463a79
Crescent Moon <t565022817@outlook.com>
2023-04-17 03:22:32 +0800

    generate enums to file

--- src/db/generator/TSEnumGenerator.ts
+++ src/db/generator/TSEnumGenerator.ts
@@ -1,6 +1,7 @@
 import {DataEnumOption} from "../definition/DataEnumOption";
 import {DataEnum} from "../definition/DataEnum";
-import {convertPackageToPath, TSImportInfo} from "../../utils/TSImportInfo";
+import {convertPackageToPath, convertPackageToSavePath, TSImportInfo} from "../../utils/TSImportInfo";
+import {exist, mkdirs, writeStringToFile} from "../../utils/FileUtils";
 
 export function generateEnumOption(def: DataEnumOption) {
     let line = `    ${def.sign} = ${def.value},`;
@@ -20,7 +21,7 @@ export function generateEnumDescConst(def: DataEnum) {
         `    [${def.name}.${prop.sign}]: '${prop.description}',`);
     return `export const ${def.name}Desc = {\n${
         fieldLines.join('\n')
-    }\n}`;
+    }\n} as Record<${def.name}, string>`;
 }
 
 export function getEnumImportInfo(def: DataEnum): TSImportInfo {
@@ -35,4 +36,34 @@ export function getEnumDescImportInfo(def: DataEnum): TSImportInfo {
         importPath: convertPackageToPath(def.package),
         importName: def.name + "Desc",
     };
+}
+
+/**
+ * 生成定义Enum和常量定义的ts文件
+ * @param defs 需保证所有定义的package相同
+ * @param subPath 子项目根路径
+ * @param genIndex 是否生成为index.ts
+ */
+export function generateEnumDefsToFile(defs: DataEnum[], subPath = "", genIndex = false) {
+    if (!defs || defs.length === 0) {
+        return;
+    }
+    let enums = defs.map(def => {
+        let enumStr = generateEnumDefine(def)
+        if (!def.ruoyiDict) {
+            let descStr = generateEnumDescConst(def);
+            enumStr += '\n\n' + descStr;
+        }
+        return enumStr;
+    });
+    let content = enums.join('\n\n');
+    let packageName = defs[0].package;
+
+    let {dirPath, fileName} = convertPackageToSavePath(packageName, subPath, genIndex);
+    let filePath = `${dirPath}/${fileName}`;
+
+    if (!exist(dirPath))
+        mkdirs(dirPath);
+
+    writeStringToFile(filePath, content);
 }
--- src/utils/TSImportInfo.ts
+++ src/utils/TSImportInfo.ts
@@ -9,3 +9,29 @@ export function convertPackageToPath(packageName: string) {
     let fePath = packageName.replace(config.basePackage, "@");
     return fePath.replace(/\./g, '/');
 }
+
+export function convertPackageToSavePath(packageName: string, subPath = "", genIndex = false) {
+    let fePath = packageName.replace(config.basePackage, "");
+
+    let pathPackage: string;
+    let fileName: string;
+    if (genIndex) {
+        pathPackage = fePath;
+        fileName = 'index.ts';
+    }
+    else {
+        let fileNameIdx = fePath.lastIndexOf('.');
+        pathPackage = fePath.substring(0, fileNameIdx);
+        fileName = `${fePath.substring(fileNameIdx + 1)}.ts`;
+    }
+    let basePath = config.baseDir;
+    if (subPath) {
+        basePath = `${basePath}\\${subPath}`;
+    }
+    let dirPath = basePath + pathPackage.replace(/\./g, '/');
+
+    return {
+        dirPath,
+        fileName,
+    }
+}
