1844ac51980c29c19d52ca6b850bc2448fb874eb
zzq <3142701787@qq.com>
2023-05-18 10:34:48 +0800

    完成filter部分的Definition的编写

--- src/frontend/view/generator/index/FilterGenerator.ts
+++ src/frontend/view/generator/index/FilterGenerator.ts
@@ -1,9 +1,22 @@
-import {FilterDefinition} from "../../definition/index/FilterDefinition";
+import {FilterDefinition, FilterFormDefinition} from "../../definition/index/FilterDefinition";
 
 export class FilterGenerator {
     static generate(filterDefinition: FilterDefinition) {
         let text = '';
+        text += FilterGenerator.generateTemplate(filterDefinition.filterFormDefinition);
+
+    }
+
+    private static generateTemplate(filterFormDefinition: FilterFormDefinition) {
+        let text = '';
+        text += '<template>';
+        text += FilterGenerator.generateFilterForm(filterFormDefinition);
+        text += '</template>';
+        return text;
+    }
 
 
+    private static generateFilterForm(filterFormDefinition: FilterFormDefinition) {
+        return "";
     }
 }
