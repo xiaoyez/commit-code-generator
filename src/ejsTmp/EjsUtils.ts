265046117e33569c390b4b5bdb0cac6dd22b4e12
Crescent Moon <t565022817@outlook.com>
2023-05-26 19:50:35 +0800

    compileEjsTmp方法缓存编译好的模板，并支持variable参数

--- src/ejsTmp/EjsUtils.ts
+++ src/ejsTmp/EjsUtils.ts
@@ -2,7 +2,20 @@ import {EjsTmp} from "./EjsTmp";
 import {template} from "lodash";
 import {readFile} from "../utils/FileUtils";
 
+let ejsFuncs = new WeakMap<EjsTmp, ReturnType<typeof template>>();
+
+function getEjsFunc(ejsTmp: EjsTmp) {
+    if (!ejsFuncs.has(ejsTmp)) {
+        const tmpStr = readFile(ejsTmp.filePath);
+        ejsFuncs.set(ejsTmp, template(tmpStr, {
+            imports: ejsTmp.imports,
+            variable: ejsTmp.variable,
+        }));
+    }
+    return ejsFuncs.get(ejsTmp)!;
+}
+
 export function compileEjsTmp(ejsTmp: EjsTmp, data: any) {
-    const tmpStr = readFile(ejsTmp.filePath);
-    return template(tmpStr, {imports: ejsTmp.imports})(data);
+    const ejsFunc = getEjsFunc(ejsTmp);
+    return ejsFunc(data);
 }
