e115a8916e75ec396e7a569d3ca6c897ffa6262e
Crescent Moon <t565022817@outlook.com>
2023-05-29 17:54:59 +0800

    fix PageGenerator.generateFilter

--- src/frontend/view/generator/page/PageGenerator.ts
+++ src/frontend/view/generator/page/PageGenerator.ts
@@ -3,10 +3,11 @@ import {compileEjsTmp} from "../../../../ejsTmp/EjsUtils";
 import {ejsTmp} from "../../../../ejsTmp/EjsTmp";
 import {TableViewDefinition} from "../../definition/page/TableViewDefinition";
 import {FormDialogDefinition} from "../../definition/page/FormDialogDefinition";
+import {filterCompViewModel} from "../../../../utils/VueComponentUtils";
 
 export class PageGenerator {
     static generateFilter(filterDefinition: FilterDefinition) {
-        return compileEjsTmp(ejsTmp.filterTmp, filterDefinition)
+        return compileEjsTmp(ejsTmp.filterTmp, filterCompViewModel(filterDefinition))
     }
 
     static generateTableView(tableViewDefinition: TableViewDefinition) {
