38d8cea1eb55236805dd6234f798cf1589953548
zzq <3142701787@qq.com>
2023-05-30 11:40:32 +0800

    重新编写实体类生成测试代码

--- test/DTOGenerator.test.ts
+++ test/DTOGenerator.test.ts
@@ -1,33 +1,10 @@
-import {ObjectTypeDefinition} from "../src/dto/definition/TypeDefinition";
-import {JavaType} from "../src/dto/definition/JavaType";
+
 import {DTOGenerator} from "../src/dto/generator/DTOGenerator";
+import {tbMemberDomainTypeDef} from "./data/DTO";
+import {JavaGeneratorUtils} from "../src/java/generator/utils/JavaGeneratorUtils";
 
 
-const objectTypeDefinition = new ObjectTypeDefinition({
-    className: "AddFriendsDTO",
-    packageName: "com.cgmanage.web.modules.ypx.domain.DTO",
-    properties: [
-        {
-            paramName: "memberId",
-            paramType: {
-                type: JavaType.Integer,
-            },
-            paramDesc: "id"
-        },
-        {
-            paramName: "friends",
-            paramType: {
-                type: JavaType.List,
-                genericTypes: [{
-                    type: JavaType.Integer
-                }]
-            },
-            paramDesc: "好友id"
-        }
-    ]
-
-})
 test('test generate',()=>{
-    DTOGenerator.generate(objectTypeDefinition);
+    DTOGenerator.generate(tbMemberDomainTypeDef);
 })
 
