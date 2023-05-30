6dcc53ef352de6141e70f4cd643217ed35b7f0a0
zzq <3142701787@qq.com>
2023-05-30 15:40:13 +0800

    重新编写Mapper接口生成测试代码

--- test/MapperGenerator.test.ts
+++ test/MapperGenerator.test.ts
@@ -1,9 +1,6 @@
-import {ObjectTypeDefinitionUtils} from "../src/dto/definition/ObjectTypeDefinitionUtils";
-import {tableUserDefinition} from "./DDLSqlGenerator.test";
 import {MapperGenerator} from "../src/dto/generator/MapperGenerator";
-
-const domainTypeDefinition = ObjectTypeDefinitionUtils.castTableCreateDefinitionToDomainTypeDefinition(tableUserDefinition);
+import {tbMemberDomainTypeDef} from "./data/DTO";
 
 test('generate mapper', () => {
-    MapperGenerator.generate(domainTypeDefinition);
+    MapperGenerator.generate(tbMemberDomainTypeDef);
 } )
