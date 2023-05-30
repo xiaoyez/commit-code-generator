0bd6b00e0807073ea6184ee74df20bc6fb0f37ca
zzq <3142701787@qq.com>
2023-05-30 15:41:21 +0800

    重新编写常量类生成测试代码

--- test/JavaConstantClassGenerator.test.ts
+++ test/JavaConstantClassGenerator.test.ts
@@ -1,30 +1,7 @@
-import {DataEnum} from "../src/db/definition/DataEnum";
-import {DataEnumOption} from "../src/db/definition/DataEnumOption";
 import {JavaConstantClassGenerator} from "../src/db/generator/JavaConstantClassGenerator";
+import {memberStatusEnumDef} from "./common";
 
-const dataEnum = new DataEnum({
-    name: "AuditStatusConstant",
-    package: "com.cgmanage.web.modules.ypx.constants",
-    options: [
-        new DataEnumOption({
-            description: "审核中",
-            sign: "AUDITING",
-            value: 1
-        }),
-        new DataEnumOption({
-            description: "通过",
-            sign: "PASS",
-            value: 2
-        }),new DataEnumOption({
-            description: "拒绝",
-            sign: "REJECT",
-            value: 3
-        }),
-    ],
-    comment: "审核状态"
-
-})
 
 test('test 根据dataEnum生成java常量类', ()=>{
-    JavaConstantClassGenerator.generateJavaConstantClass(dataEnum)
+    JavaConstantClassGenerator.generateJavaConstantClass(memberStatusEnumDef)
 })
