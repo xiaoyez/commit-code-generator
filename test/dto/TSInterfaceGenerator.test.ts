de5f81e3074ea4a736acd0a7e95509f27e98d33a
zzq <3142701787@qq.com>
2023-05-31 11:28:56 +0800

    重新编写TSInterfaceGenerator的测试

--- test/dto/TSInterfaceGenerator.test.ts
+++ test/dto/TSInterfaceGenerator.test.ts
@@ -1,86 +1,11 @@
-import {
-    generateInterfaceDefine,
-    generateInterfaceDefsToFile,
-} from "../../src/dto/generator/TSInterfaceGenerator";
-import {JavaType} from "../../src/dto/definition/JavaType";
-import {ObjectTypeDefinition, TypeDefinition} from "../../src/dto/definition/TypeDefinition";
-import {config} from "../../src/config/Config";
-import {auditStatusEnumDef} from "../common";
+import {generateInterfaceDefsToFile} from "../../src/dto/generator/TSInterfaceGenerator";
+import {tbMemberDomainTypeDef, TbMemberSearchDTODef} from "../data/DTO";
+import {ObjectTypeDefinition} from "../../src/dto/definition/TypeDefinition";
 
 describe('TSInterfaceGenerator', () => {
 
-    let interfaceDef = new ObjectTypeDefinition({
-        className: 'TestResp',
-        packageName: `${config.projectPackage}.${config.dtoPackage}.testOther`,
-        properties: [
-            {
-                paramName: "fieldA",
-                paramType: new TypeDefinition({
-                    type: JavaType.Long,
-                }),
-                paramDesc: "bigInt field",
-            },
-            {
-                paramName: "fieldB",
-                paramType: new TypeDefinition({
-                    type: JavaType.Float,
-                }),
-            },
-            {
-                paramName: "fieldC",
-                paramType: new TypeDefinition({
-                    type: JavaType.Integer,
-                }),
-                enumType: auditStatusEnumDef,
-            }
-        ]
-    });
-
-    it('generate interface define', () => {
-        expect(generateInterfaceDefine(interfaceDef)).toBe(`export interface TestResp {
-    /** bigInt field */
-    fieldA: bigint;
-    fieldB: number;
-    fieldC: AuditStatusConstant;
-}`);
-    });
-
-    let testPackage = `${config.projectPackage}.${config.dtoPackage}.test`;
-    let moduleDefs = [
-        new ObjectTypeDefinition({
-            className: "TypeA",
-            packageName: testPackage,
-            properties: [
-                {
-                    paramName: "fieldA", paramType: new TypeDefinition({
-                        type: JavaType.List, genericTypes: [{type: JavaType.Date}]
-                    })
-                },
-                {paramName: "fieldB", paramType: new TypeDefinition({type: JavaType.Integer})},
-            ]
-        }),
-        new ObjectTypeDefinition({
-            className: "TypeB",
-            packageName: testPackage,
-            properties: [
-                {paramName: "fieldC", paramType: new TypeDefinition({type: interfaceDef})},
-            ]
-        }),
-        new ObjectTypeDefinition({
-            className: "TypeC",
-            packageName: testPackage,
-            properties: [
-                {
-                    paramName: "fieldD",
-                    paramType: new TypeDefinition({type: JavaType.Integer}),
-                    enumType: auditStatusEnumDef,
-                },
-            ]
-        }),
-    ]
-
     it('generate to file', () => {
-        generateInterfaceDefsToFile([interfaceDef]);
-        generateInterfaceDefsToFile(moduleDefs);
+        generateInterfaceDefsToFile([tbMemberDomainTypeDef, (TbMemberSearchDTODef.type as ObjectTypeDefinition)]);
+        // TODO: 生成文件的时候套了两层dto
     });
 });
