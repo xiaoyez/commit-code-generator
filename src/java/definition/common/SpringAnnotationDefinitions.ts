705e2080b6a9c26e4c3905844c8a36c7a0c0ec82
zzq <3142701787@qq.com>
2023-05-31 14:58:27 +0800

    重构RequestMapping相关注解，当path为空串时不给注解传属性值对

--- src/java/definition/common/SpringAnnotationDefinitions.ts
+++ src/java/definition/common/SpringAnnotationDefinitions.ts
@@ -7,16 +7,16 @@ export const SpringAnnotationDefinitions = {
         return new AnnotationDefinition('org.springframework.web.bind.annotation','RequestMapping', [{name: 'value', value: path}]);
     },
     GetMapping(path: string): AnnotationDefinition {
-        return new AnnotationDefinition('org.springframework.web.bind.annotation','GetMapping', [{name: 'value', value: path}]);
+        return new AnnotationDefinition('org.springframework.web.bind.annotation','GetMapping', path?[{name: 'value', value: path}]: []);
     },
     PostMapping(path: string): AnnotationDefinition {
-        return new AnnotationDefinition('org.springframework.web.bind.annotation','PostMapping', [{name: 'value', value: path}]);
+        return new AnnotationDefinition('org.springframework.web.bind.annotation','PostMapping', path?[{name: 'value', value: path}]: []);
     },
     PutMapping(path: string): AnnotationDefinition {
-        return new AnnotationDefinition('org.springframework.web.bind.annotation','PutMapping', [{name: 'value', value: path}]);
+        return new AnnotationDefinition('org.springframework.web.bind.annotation','PutMapping', path?[{name: 'value', value: path}]: []);
     },
     DeleteMapping(path: string): AnnotationDefinition {
-        return new AnnotationDefinition('org.springframework.web.bind.annotation','DeleteMapping', [{name: 'value', value: path}]);
+        return new AnnotationDefinition('org.springframework.web.bind.annotation','DeleteMapping', path?[{name: 'value', value: path}]: []);
     },
     RequestBody: new AnnotationDefinition('org.springframework.web.bind.annotation','RequestBody'),
     Autowired: new AnnotationDefinition('org.springframework.beans.factory.annotation','Autowired'),
