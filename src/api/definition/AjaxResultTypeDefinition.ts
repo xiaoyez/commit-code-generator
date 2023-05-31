34032bd523af9e42bf5ec0b256cd0e6edf044ab0
zzq <3142701787@qq.com>
2023-05-31 10:36:53 +0800

    新增将AjaxResultTypeDefinition和TableDataInfoTypeDefinition转成ClassDefinition的方法

--- src/api/definition/AjaxResultTypeDefinition.ts
+++ src/api/definition/AjaxResultTypeDefinition.ts
@@ -1,6 +1,8 @@
 import {ITypeDefinition, ObjectTypeDefinition, TypeDefinition} from "../../dto/definition/TypeDefinition";
 import {config} from "../../config/Config";
 import {JavaType} from "../../dto/definition/JavaType";
+import {ClassDefinition} from "../../java/definition/ClassDefinition";
+import {DTOGenerator} from "../../dto/generator/DTOGenerator";
 
 // AjaxResult
 const ajaxResult = new ObjectTypeDefinition({
@@ -31,4 +33,15 @@ export class AjaxResultTypeDefinition extends TypeDefinition {
     static createAjax(genericType?: JavaType|ObjectTypeDefinition) {
         return new AjaxResultTypeDefinition({genericType: genericType?{type: genericType}: undefined});
     }
+}
+
+export function castAjaxResultTypeDefinitionToClassDefinition(definition: AjaxResultTypeDefinition) {
+    const ajaxResultClassDefinition = new ClassDefinition( `${config.projectPackage}.core.domain` ,'AjaxResult');
+    if (definition.genericTypes)
+    {
+        definition.genericTypes.forEach(genericType => {
+            ajaxResultClassDefinition.addGenericType(DTOGenerator.castToClassDefinition(genericType.type as ObjectTypeDefinition));
+        });
+    }
+    return ajaxResultClassDefinition;
 }
--- src/api/definition/TableDataInfoTypeDefinition.ts
+++ src/api/definition/TableDataInfoTypeDefinition.ts
@@ -1,6 +1,8 @@
 import {ITypeDefinition, ObjectTypeDefinition, TypeDefinition} from "../../dto/definition/TypeDefinition";
 import {config} from "../../config/Config";
 import {JavaType} from "../../dto/definition/JavaType";
+import {ClassDefinition} from "../../java/definition/ClassDefinition";
+import {DTOGenerator} from "../../dto/generator/DTOGenerator";
 
 // TableDataInfo
 const tableDataInfo = new ObjectTypeDefinition({
@@ -31,4 +33,15 @@ export class TableDataInfoTypeDefinition extends TypeDefinition {
     static createTableDataInfo(genericType: JavaType|ObjectTypeDefinition) {
         return new TableDataInfoTypeDefinition({genericType: {type: genericType}});
     }
+}
+
+export function castTableDataInfoTypeDefinitionToClassDefinition(definition: TableDataInfoTypeDefinition) {
+    const tableDataInfoClassDefinition = new ClassDefinition( `${config.projectPackage}.core.page` ,'TableDataInfo');
+    if (definition.genericTypes)
+    {
+        definition.genericTypes.forEach(genericType => {
+            tableDataInfoClassDefinition.addGenericType(DTOGenerator.castToClassDefinition(genericType.type as ObjectTypeDefinition));
+        });
+    }
+    return tableDataInfoClassDefinition;
 }
